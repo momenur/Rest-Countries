26d62c0efefd91f6914c2746df9fe8e88a52a5f4
momenur <momenurislam6@gmail.com>
2023-03-21 16:23:21 +0600

    some bug fix

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const loadCountries = () => {
     .then(data => DisplayCountries(data))
 };
 const DisplayCountries = countries => {
+    console.log(countries[0])
     const allCountriesHTML = countries.map(country => getCountryHTML(country))
     const container = document.getElementById('countries');
     container.innerHTML = allCountriesHTML.join(' ');
@@ -13,8 +14,9 @@ const getCountryHTML = country =>{
         <div class="country">
             <img src="${country.flags.png}" class="flags-images">
             <h2>Country Name: <span class="country-name">${country.name.common}</span></h2>
-            <h4>Capital Name: ${country.capital}</h4>
-            
+            <h4>Capital Name: ${country.capital?country.capital:'NO Capital'}</h4>
+            <h4>Area: ${country.area}</h4>
+
         </div>
     `
 }
